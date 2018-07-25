6e8fb543748387ee8ee90f05d49d076fec3d6414
Thomas-Maisonneuve <maisonneuve.thoma@gmail.com>
2018-07-25 14:10:00 +0200

    Mail reset password

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\LostPassword;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -49,5 +50,100 @@ class SecurityController extends Controller
         return $this->render('security/register.html.twig',
             array('form' =>$form->createView())
         );
+    }
+
+    /**
+     * @Route("/reset-password", name="reset-password")
+     */
+    public function resetPassword(Request $request,\Swift_Mailer $mailer)
+    {
+        $email = $request->request->get('email', 0);
+        if($email){
+            //verifier que l'email existe dans ma bdd
+            $repository=$this->getDoctrine()->getRepository(User::class);
+            $user=$repository->searchEmail($email);
+            //dump($user);
+            if($user){
+                //l'email existe
+                $resetPass = new LostPassword();
+                $token = md5(uniqid(rand(),true));
+                //je rentre dans la bdd mon token et le user_id
+                $resetPass->setToken($token);
+                $resetPass->setUser($user['0']);
+                $entityManager = $this->getDoctrine()->getManager();
+                $entityManager->persist($resetPass);
+                $entityManager->flush();
+                $user_id = $user['0']->getId(); // avec getId je recupere l'element
+                dump($user_id);
+                $message = (new \Swift_Message('Reset Mot de Passe'))
+                    ->setFrom('youtwitch123@gmail.com')
+                    ->setTo($email)
+                    ->setBody(
+                        $this->renderView(
+                        // templates/emails/registration.html.twig
+                            'security/resetPassword.html.twig',
+                            array('token' => $token, 'idUser' => $user_id)
+                        ),
+                        'text/html'
+                    );
+                if ($mailer->send($message)) {
+                    $this->addFlash('success', "L'e-mail a bien été envoyé !");
+                }else{
+                    $this->addFlash('danger',"Erreur lors de l'envoi de l'e-mail veuillez verifier votre adresse e-mail.");
+                }
+            }else{
+                $this->addFlash('danger',"Ton e-mail n'existe pas dons notre base de données.");
+            }
+        }
+        return $this->render('user/askEmail.html.twig');
+    }
+    /**
+     * @Route("/new-password", name="new-password")
+     */
+    public function newPassword(Request $request)
+    {
+        $token = $request->query->get('token', 0);
+        $user_id = $request->query->get('iduser', 0);
+        if($token && $user_id){
+            //verifier que le token et le user id existe dans ma bdd
+            $repository=$this->getDoctrine()->getRepository(LostPassword::class);
+            $resetPassword=$repository->searchToken($token,$user_id);
+            if(!$resetPassword){
+                //token et id incorrects donc redirectionner
+                $this->addFlash('danger',"Erreur !! Paramètres invalides");
+                return $this->redirectToRoute('reset-password');
+            }
+        }else{
+            $this->addFlash('error',"ERREUR !!");
+        }
+        return $this->render('security/newPassword.html.twig', array('idUser'=>$user_id) );
+    }
+    /**
+     * @Route("/ResetPasswordOk", name="reset-password-ok")
+     */
+    public function modifPassword(Request $request, UserPasswordEncoderInterface $encoder)
+    {
+        //ici je vais faire la modification de mot de passe
+        $password = $request->request->get('password', 0);
+        //metre en cript le mdp
+        $idUser = $request->request->get('idUser');
+        $user = $this->getDoctrine()->getRepository(User::class )->find($idUser);
+        if($user){
+            $mdpEncoded = $encoder->encodePassword($user, $password);
+        }
+        if($password){
+            $repository=$this->getDoctrine()->getRepository(User::class);
+            $user=$repository->find($idUser);
+            if($user){
+                $user->setpassword($mdpEncoded);
+                $entityManager = $this->getDoctrine()->getManager();
+                $entityManager->persist($user);
+                $entityManager->flush();
+                $this->addFlash('success', "Mot de passe modifié !");
+            }else{
+                $this->addFlash('danger',"Le mot de passe n'a pa pu être modifié");
+            }
+        }
+        return $this->render('security/passwordUpdate.html.twig');
     }
 }
---
+++ src/Repository/LostPasswordRepository.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\LostPassword;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+/**
+ * @method LostPassword|null find($id, $lockMode = null, $lockVersion = null)
+ * @method LostPassword|null findOneBy(array $criteria, array $orderBy = null)
+ * @method LostPassword[]    findAll()
+ * @method LostPassword[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class LostPasswordRepository extends ServiceEntityRepository
+{
+    public function __construct(RegistryInterface $registry)
+    {
+        parent::__construct($registry, LostPassword::class);
+    }
+
+    # Requête pour chercher le token et l'ID dans la BDD
+    public function searcheToken(string $token, int $idUser)
+    {
+        $querybuilder = $this->createQueryBuilder('r')
+            ->andWhere('r.token = :token','r.user_id = :userId')
+            ->setParameter('token', $token)
+            ->setParameter('userId',$idUser)
+            ->getQuery();
+        return $querybuilder->execute();
+    }
+
+//    /**
+//     * @return LostPassword[] Returns an array of LostPassword objects
+//     */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('l')
+            ->andWhere('l.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('l.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?LostPassword
+    {
+        return $this->createQueryBuilder('l')
+            ->andWhere('l.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
