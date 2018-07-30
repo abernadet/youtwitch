074fc57fdc824fb6433ec4ba0a6bfe62248ee3bd
abernadet <40197577+abernadet@users.noreply.github.com>
2018-07-30 15:24:02 +0200

    Scrollbar sur la section "En live" de la sidebar
    CSS de la scrollbar dans public/css/style.css

--- src/Controller/AjaxController.php
+++ src/Controller/AjaxController.php
@@ -18,50 +18,60 @@ class AjaxController extends Controller
         $displayed_follows = 3;
 
         $login = $user->getTwitchLogin();
-        $user_id = $twitch_api->getUserIdFromLogin($login);
-        $follows_id =  $twitch_api->getRandomUserFollowsId($user_id, $displayed_follows);
-        $followed_users = $twitch_api->getUsersFromId($follows_id);
 
-        $live_streams = ($twitch_api->getLiveStreams($follows_id))->data;
-        $live_streams_user_id = [];
-        foreach($live_streams as $stream)
+        if($login != null)
         {
-            $live_streams_user_id[] = $stream->user_id;
-        }
+            $user_id = $twitch_api->getUserIdFromLogin($login);
+            $follows_id =  $twitch_api->getRandomUserFollowsId($user_id, $displayed_follows);
+            $followed_users = $twitch_api->getUsersFromId($follows_id);
 
-
-        $clips = [];
-        $replays = [];
-        $data = [];
-
-        foreach($followed_users as $followed_user)
-        {
-            
-            $clips_full = $twitch_api->getClipsFromChannel($followed_user->login, 'month', 4);
-            $clips[$followed_user->login] = $clips_full->clips;
-            $replays[$followed_user->login] = $twitch_api->getVideosFromChannel($followed_user->id, 'month', 4, 'time');
-            
-            $data[$followed_user->login] = [
-                'clip_data' => $clips[$followed_user->login], 
-                'replay_data' => $replays[$followed_user->login]->data, 
-                'display_name' => $followed_user->display_name, 
-                'user_id' => $followed_user->id, 
-                'login' => $followed_user->login,
-            ];
-
-            foreach($data[$followed_user->login]['replay_data'] as $replay)
+            $live_streams = ($twitch_api->getLiveStreams($follows_id))->data;
+            $live_streams_user_id = [];
+            foreach($live_streams as $stream)
             {
-                $replay->thumbnail_url = $text_format->format_twitch_video_thumbnail_url($replay->thumbnail_url);
+                $live_streams_user_id[] = $stream->user_id;
             }
-        }
 
-        
+
+            $clips = [];
+            $replays = [];
+            $data = [];
+
+            foreach($followed_users as $followed_user)
+            {
+                
+                $clips_full = $twitch_api->getClipsFromChannel($followed_user->login, 'month', 4);
+                $clips[$followed_user->login] = $clips_full->clips;
+                $replays[$followed_user->login] = $twitch_api->getVideosFromChannel($followed_user->id, 'month', 4, 'time');
+                
+                $data[$followed_user->login] = [
+                    'clip_data' => $clips[$followed_user->login], 
+                    'replay_data' => $replays[$followed_user->login]->data, 
+                    'display_name' => $followed_user->display_name, 
+                    'user_id' => $followed_user->id, 
+                    'login' => $followed_user->login,
+                ];
+
+                foreach($data[$followed_user->login]['replay_data'] as $replay)
+                {
+                    $replay->thumbnail_url = $text_format->format_twitch_video_thumbnail_url($replay->thumbnail_url);
+                }
+            }
+            
+            
+        }
+        else
+        {
+            $data = null;
+            $live_streams_user_id = null;
+            $followed_users = null;
+        }
         
         return $this->render('ajax/twitch.html.twig', [
             'data' => $data,
             'streams_user_id' => $live_streams_user_id,
             'followed_users' => $followed_users
-        ]);
+            ]);
     }
 
     /**
