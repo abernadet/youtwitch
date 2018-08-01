b25d41b66da56d582e04124ecca488fd38bff3c8
Thomas-Maisonneuve <maisonneuve.thoma@gmail.com>
2018-08-01 14:01:16 +0200

    mise en page messagerie

--- src/Migrations/Version20180801104322.php
+++ src/Migrations/Version20180801104322.php
@@ -8,13 +8,14 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20180801081703 extends AbstractMigration
+final class Version20180801104322 extends AbstractMigration
 {
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
+        $this->addSql('ALTER TABLE user ADD YoutubeLogin VARCHAR(255) DEFAULT NULL');
         $this->addSql('ALTER TABLE message ADD sujet VARCHAR(255) NOT NULL');
     }
 
@@ -24,5 +25,6 @@ final class Version20180801081703 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE message DROP sujet');
+        $this->addSql('ALTER TABLE user DROP YoutubeLogin');
     }
 }
